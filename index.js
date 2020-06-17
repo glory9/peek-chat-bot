bf920cc1fca8016ff4c71afc5b3ec277dfaaa71b
Glory Adedayo <gloryadedayo10@gmail.com>
2020-06-17 04:07:03 -0500

    commit test code

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const express = require('express');
+const body_parser = require('body-parser');
+const request = require('request');
+
+const bot = express();
+
+bot.set('port', (process.env.PORT || 5000));
+bot.use(bodyParser.urlencoded({ extended: false }));
+bot.use(bodyParser.json());
+
+bot.get('/', function(req, res) {
+    res.send("Welcome to Peek's FaceBook Chatbot App")
+});
+
+bot.get('/webhook/', function(req, res) {
+    if (req.query['hub.verify_token'] === "thispeekbot") {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send("Wrong token")
+})
+
+bot.listen(bot.get('port'), function() {
+    console.log("running: port")
+});
