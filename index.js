89dab46bb072898d3d63b0a45e555150b636291a
Glory Adedayo <gloryadedayo10@gmail.com>
2020-06-17 04:15:49 -0500

    fixed bugs

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const request = require('request');
 const bot = express();
 
 bot.set('port', (process.env.PORT || 5000));
-bot.use(bodyParser.urlencoded({ extended: false }));
-bot.use(bodyParser.json());
+bot.use(body_parser.urlencoded({ extended: false }));
+bot.use(body_parser.json());
 
 bot.get('/', function(req, res) {
     res.send("Welcome to Peek's FaceBook Chatbot App")
