bfd28cea4dac0a876add05e58f39182589acdc86
Glory Adedayo <gloryadedayo10@gmail.com>
2020-06-18 10:03:52 -0500

    fixed request

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ bot.post('/webhook', (req, res) => {
         }
 
         // Return a '200 OK' response to all events
-        res.status(200).send('EVENT_RECEIVED');
+        //res.status(200).send('EVENT_RECEIVED');
 
     } else {
         // Return a '404 Not Found' if event is not from a page subscription
