1ef1695f6a7665031a93f0cabe65e918592c4fef
Glory Adedayo <53071086+glory9@users.noreply.github.com>
2020-06-23 02:08:26 -0500

    Update rough.sh

--- rough.sh
+++ rough.sh
@@ -10,4 +10,4 @@ curl -X POST -H "Content-Type: application/json" -d '{
     "payload":"first time user"
   }
 }
-}' "https://graph.facebook.com/v7.0/me/messenger_profile?access_token=EAAD1vUUipicBAAgoASWGSYGGx6b3exxjM12hUiE61pgU1qMzf1isXPWdN6STo2u417brWKezcFJSHLuC84o6V83BTLPYhyqawJAmLa4k1CkV9jThU581R3FGYQo78QMCG3jdb6SZADGjEZBMEFb2j2T5k8DbY8ZCmZBdHy11IGBzaEowqlFH"
+}' "https://graph.facebook.com/v7.0/me/messenger_profile?access_token=""
